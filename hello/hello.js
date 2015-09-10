b154903b5d3810395fac91bf11c1c590d6b7137c
Bruno Bossola <bbossola@gmail.com>
2015-09-10 13:37:33 +0100

    Now hello is using the registry also :)

--- hello/hello.js
+++ hello/hello.js
@@ -4,16 +4,22 @@ var rest = require('unirest');
 var http = require('http');
 var app = express();  
 
+var port_time;
+var port_rand;
+
 app.get("/", function(request, response) { 
-    console.log("call received!");
+    console.log("Call received!");
+	console.log("Will call \"time\" at port "+port_time)
+	console.log("Will call \"rand\" at port "+port_rand)
+
     async.parallel({
             time: function(callback) {
-                rest.get("http://localhost:3002").end(function(res) {
+                rest.get("http://localhost:"+port_time).end(function(res) {
                     callback(null, res.body);
                 })
             },
             rand: function(callback) {
-                rest.get("http://localhost:3003").end(function(res) {
+                rest.get("http://localhost:"+port_rand).end(function(res) {
                     callback(null, res.body);
                 })
             }
@@ -38,3 +44,12 @@ app.listen(3001, function() {                       
     console.log("Hello service started on port 3001");
 });
 
+setInterval(function() {
+    rest.get("http://localhost:3000/time/").end(function(res) {
+        port_time = res.body.port;
+    });
+
+    rest.get("http://localhost:3000/rand/").end(function(res) {
+        port_rand = res.body.port;
+    });
+}, 2500);
